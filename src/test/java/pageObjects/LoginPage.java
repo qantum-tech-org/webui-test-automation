964ca4a2885b50df7f4bcb1e8e0cacbb98428ded
Pedro Porras <pedroporrasmedina@gmail.com>
2022-12-07 17:10:53 -0600

    Updates applied for the Login Test, pending to optimize the web element selection process.

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,26 +1,41 @@
 package pageObjects;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
 
-public class  LoginPage {
+import java.util.List;
+
+public class LoginPage {
     WebDriver driver;
-    @BeforeTest
+    @BeforeMethod
     public void starUpBrowser(){
        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
         driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/login");
     }
     @Test
-    public void loginForm(){
-        System.out.println("wenas");
+    public void loginForm() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("#login > button")).click();
+        Thread.sleep(3000);
+        String loginTextConfirmation = driver.findElement(By.className("subheader")).getText();
+        String logoutBtn = driver.findElement(By.partialLinkText("Logout")).getText();
+        SoftAssert loginPageConfirm = new SoftAssert();
+        loginPageConfirm.assertEquals(loginPageConfirm,"Welcome to the Secure Area. When you are done click logout below.","Login confirmation was successfull");
+        loginPageConfirm.assertEquals(logoutBtn,"Logout","Login confirmation was successfull");
+        driver.findElement(By.partialLinkText("Logout")).click();
+        driver.findElement(By.cssSelector("#login > button")).isDisplayed();
+        Thread.sleep(3000);
     }
-    @AfterTest
+
+    @AfterMethod
     public void closeBrowser(){
         System.out.println("Closing down the browser");
-        driver.close();
+      driver.close();
     }
 }
